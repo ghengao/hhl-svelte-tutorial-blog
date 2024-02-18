bc59bb4d90a84db34e0e9f66ee736f76b7b6da26
Heng GAO <heng.gao.us@gmail.com>
2024-02-18 10:38:36 -0600

    add styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,36 @@
 <script lang="ts">
 	import '../app.pcss';
+	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
 </script>
 
-<slot />
+<div class="layout">
+	<Header />
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</div>
+
+<style>
+    .layout {
+        height: 100%;
+        max-inline-size: 1440px;
+        display: grid;
+        grid-template-rows: auto 1fr auto;
+        margin-inline: auto;
+        padding-inline: var(--size-7);
+
+    }
+
+    main {
+        padding-block: var(--size-9);
+    }
+
+    @media (min-width: 1440px) {
+        .layout {
+            padding-inline: 0;
+        }
+    }
+
+</style>
